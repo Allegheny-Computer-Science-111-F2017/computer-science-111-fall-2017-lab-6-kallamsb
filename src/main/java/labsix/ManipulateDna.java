fce80c51a923b0ccee6b8dd771aaf445e5d5a441
Brandon Kallams <kallamsb@aldenv178.allegheny.edu>
2017-10-12 15:46:38 -0400

    commits for lab 6

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,24 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
+/**
+*This is lab 6 source code. For this lab the programmer is to explore
+*bioinformatics. More specifically the programmer must manipulate
+*DNA.
+*/
 public class ManipulateDna {
 
+  /**
+  *This source code is used to create a complement string of dna from a string
+  *that is read in from a file. The code must use a replace command in order to
+  *change the input letters to their complments.
+  */
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Brandon Kallams " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,17 +32,58 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String dna = scanner.nextLine();
+    dna = dna.toLowerCase();
+    System.out.println("The Chosen DNA strand is " + dna);
     // Step Two: Compute the complement of the DNA String
-
+    String dnaComp =  dna.replace('a','T');
+    dnaComp = dnaComp.replace('t','A');
+    dnaComp = dnaComp.replace('c','G');
+    dnaComp = dnaComp.replace('g','C');
+    dna = dna.toUpperCase();
+    System.out.println("The Complement of " + dna + " is " + dnaComp);
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
-
+    Random ranD = new Random();
+    char newchar;
+    int num = ranD.nextInt(4);
+    if (num == 0) {
+      newchar = 'A';
+    } else if (num == 1) {
+      newchar = 'T';
+    } else if (num == 2) {
+      newchar = 'C';
+    } else {
+      newchar = 'G';
+    }
+    System.out.println("This is an example of an insertion at position 0:");
+    System.out.println(newchar + dna);
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
+    int deleTion = ranD.nextInt(dna.length());
+    String substring = dna.substring(0, deleTion);
+    String substring1 = dna.substring(deleTion + 1);
+    System.out.println("This is a deletion at position " + deleTion + ":");
+    System.out.println(substring + substring1);
 
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
+    char swapChar;
+    int num1 = ranD.nextInt(4);
+    if (num1 == 0) {
+      swapChar = 'A';
+    } else if (num1 == 1) {
+      swapChar = 'T';
+    } else if (num1 == 2) {
+      swapChar = 'C';
+    } else {
+      swapChar = 'G';
+    }
+    int charSwap = ranD.nextInt(dna.length());
+    String substring2 = dna.substring(0, charSwap);
+    String substring3 = dna.substring(charSwap + 1);
+    System.out.println("This is a letter swap at position " + charSwap + ":");
+    System.out.println(substring2 + swapChar + substring3);
 
     // Step Six: Display a final thankyou message
-
+    System.out.println("Thank you for exploring Bioinformatics! " + "\n" + "Have a nice day.");
   }
 
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,26 @@
-# Reflection by
+# Reflection by Brandon Kallams
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+We first started lab 6 last Thursday October 5th. Last Thursday, I completed
+the first two steps of the lab. In those steps, I read in and displayed the
+chosen DNA string, then I computed the complement of the DNA strand. I then
+printed out the dna strand and its complement. When computing the complement, I
+did the complement as capital letters and the original as lower case letters,
+so at each step I could tell the difference between replaced letters and
+original letters. After that, I was finished for the day.
+
+Now, on today October 12th, I am starting step three first. For step three I
+wrote a series of if and else if statements in order to choose a random DNA
+letter to insert into the dna strand. I chose to insert that randomly chosen
+letter at the beginning of the strand. Then, for step four, I created a integer
+variable to randomly choose one of the letters in the given DNA string. The
+randomly chosen letter is then deleted and the resulting DNA string is
+displayed. Lastly, for step five, I kind of combined steps three and four. I
+again generated a random number. Then I randomly chose a position in the DNA
+string and deleted the letter that currently existed in that spot and added in
+the letter I had randomly generated. I then displayed the new DNA string with
+the position where the swapping took place labled as such.
+
+Lab 6 went relatively smooth for me, however where I neede the most help was
+randomly determing the positions where the mutations should take place. Once, I
+asked a ta for help, I was able to figure it out and continue to roll on my
+way.
